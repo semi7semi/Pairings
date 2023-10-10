35c2f5eaf9b13fa58a8617d3b29f4b652ad8d55a
semi7semi <sommer.marcin@gmail.com>
2023-10-10 12:28:58 +0200

    parings color

--- Tabelka/views.py
+++ Tabelka/views.py
@@ -91,8 +91,40 @@ class Pairing5v5View(View):
         pairing = Team_of_5.objects.get(pk=p_id)
         teamA = [tournament.p1, tournament.p2, tournament.p3, tournament.p4, tournament.p5]
         teamB = [pairing.op1, pairing.op2, pairing.op3, pairing.op4, pairing.op5]
+        p1 = [pairing.p11, pairing.p12, pairing.p13, pairing.p14, pairing.p15]
+        p2 = [pairing.p21, pairing.p22, pairing.p23, pairing.p24, pairing.p25]
+        p3 = [pairing.p31, pairing.p32, pairing.p33, pairing.p34, pairing.p35]
+        p4 = [pairing.p41, pairing.p42, pairing.p43, pairing.p44, pairing.p45]
+        p5 = [pairing.p51, pairing.p52, pairing.p53, pairing.p54, pairing.p55]
+        sum_p1 = sum(p1)
+        sum_p2 = sum(p2)
+        sum_p3 = sum(p3)
+        sum_p4 = sum(p4)
+        sum_p5 = sum(p5)
+        col1 = [pairing.p11, pairing.p21, pairing.p31, pairing.p41, pairing.p51]
+        col2 = [pairing.p12, pairing.p22, pairing.p32, pairing.p42, pairing.p52]
+        col3 = [pairing.p13, pairing.p23, pairing.p33, pairing.p43, pairing.p53]
+        col4 = [pairing.p14, pairing.p24, pairing.p34, pairing.p44, pairing.p54]
+        col5 = [pairing.p15, pairing.p25, pairing.p35, pairing.p45, pairing.p55]
+        sum_col1 = sum(col1)
+        sum_col2 = sum(col2)
+        sum_col3 = sum(col3)
+        sum_col4 = sum(col4)
+        sum_col5 = sum(col5)
+        tab_pairing_points = [
+            [p1, sum_p1],
+            [p2, sum_p2],
+            [p3, sum_p3],
+            [p4, sum_p4],
+            [p5, sum_p5],
+        ]
+        tab_col_points = [sum_col1, sum_col2, sum_col3, sum_col4, sum_col5]
+
+
         ctx = {
             "tournament": tournament,
             "pairing": pairing,
+            "tab_pairing_points": tab_pairing_points,
+            "tab_col_points": tab_col_points,
         }
         return render(request, "pairing5v5.html", ctx)
